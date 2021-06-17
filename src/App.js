ad51e7f81432f16950dcce8f8340cc8ca7ab5cb2
MD Shimul <mdshemul480@gmail.com>
2021-06-17 17:22:46 +0600

    adding header to the checkout page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className="App">
         <Switch>
           <Route path="/checkout">
+            <Header />
             <h1>checkout</h1>
           </Route>
           <Route path="/login">
