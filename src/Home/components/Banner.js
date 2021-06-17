05a3394b78ee84dc4cf97a9f66a6f3fbe3eb1c0e
MD Shimul <mdshemul480@gmail.com>
2021-06-17 18:15:10 +0600

    updating image

--- src/Home/components/Banner.js
+++ src/Home/components/Banner.js
@@ -4,7 +4,7 @@ import classes from './Banner.module.css'
 function Banner() {
     return (
         <Fragment>
-            <img className={classes.home__image} src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_TallHero_Gamers_en_US_1x._CB667161802_.jpg" alt="" />
+            <img className={classes.home__image} src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="" />
         </Fragment>
     )
 }
