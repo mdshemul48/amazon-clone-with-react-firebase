fcce7680535dd2296c4020fd55f41731ef49520d
MD Shimul <mdshemul480@gmail.com>
2021-06-17 13:47:16 +0600

    adding header folders, file

---
+++ src/Header/page/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+function Header() {
+    return (
+        <div>
+            {/* logo on the left -> img */}
+            {/* search box  */}
+            {/* 3 links  */}
+            {/* basket item with number */}
+        </div>
+    )
+}
+
+export default Header;
