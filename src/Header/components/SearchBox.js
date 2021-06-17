5cbddbb1622e196136bec753f6e3c2846d0f0054
MD Shimul <mdshemul480@gmail.com>
2021-06-17 14:51:52 +0600

    adding new search box

---
+++ src/Header/components/SearchBox.js
@@ -0,0 +1,11 @@
+import { Fragment } from 'react'
+
+const SearchBox = () => {
+    return (
+        <Fragment>
+            <input type="text" className="header__searchInput" />
+        </Fragment>
+    )
+}
+
+export default SearchBox;
