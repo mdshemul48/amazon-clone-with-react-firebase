2740cefac916313d97bfede7f1460184008e6362
MD Shimul <mdshemul480@gmail.com>
2021-06-17 14:39:12 +0600

    adding logo compnent

---
+++ src/shared/components/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+import classes from "./Logo.module.css"
+
+const Logo = () => {
+    return (
+        <div>
+            <img className={classes.header__logo} src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+        </div>
+    )
+}
+
+
+export default Logo;
