48a2f07a7aac1784b62515fffcb1312ef479a539
MD Shimul <mdshemul480@gmail.com>
2021-06-17 14:25:24 +0600

    adding amazon logo to the site

--- src/Header/page/Header.js
+++ src/Header/page/Header.js
@@ -1,15 +1,19 @@
 import React from 'react'
 
 
+// Header Component
+// this is the main header of the site.
 
 function Header() {
     return (
-        <div>
+        <nav className="header">
             {/* logo on the left -> img */}
+            <img className="header__logo" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+
             {/* search box  */}
             {/* 3 links  */}
             {/* basket item with number */}
-        </div>
+        </nav>
     )
 }
 
