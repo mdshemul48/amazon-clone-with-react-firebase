43d436942fb1c845267db020729b042087edf252
MD Shimul <mdshemul480@gmail.com>
2021-06-17 20:06:02 +0600

    creating static content

--- src/Home/pages/Home.js
+++ src/Home/pages/Home.js
@@ -3,11 +3,28 @@ import React from 'react'
 
 // castom components 
 import Banner from '../components/Banner';
+import Product from '../components/Product';
+// castom css
 import classes from './Home.module.css';
 function Home() {
     return (
         <div className={classes.home}>
             <Banner />
+
+            {/* product id, title, price, rating, image */}
+            {/* product */}
+            <div className={classes.home__row}>
+                <Product id={234234} title="Roku Streaming Stick+ | HD/4K/HDR Streaming Device with Long-range Wireless and Voice Remote with TV Controls" price={39.00} rating={5} image="https://images-na.ssl-images-amazon.com/images/I/81lsA5v8EIL._AC_SL1500_.jpg" />
+                <Product id={234234} title="Roku Streaming Stick+ | HD/4K/HDR Streaming Device with Long-range Wireless and Voice Remote with TV Controls" price={39.00} rating={5} image="https://images-na.ssl-images-amazon.com/images/I/81lsA5v8EIL._AC_SL1500_.jpg" />
+            </div>
+            <div className={classes.home__row}>
+                <Product id={234234} title="Roku Streaming Stick+ | HD/4K/HDR Streaming Device with Long-range Wireless and Voice Remote with TV Controls" price={39.00} rating={5} image="https://images-na.ssl-images-amazon.com/images/I/81lsA5v8EIL._AC_SL1500_.jpg" />
+                <Product id={234234} title="Roku Streaming Stick+ | HD/4K/HDR Streaming Device with Long-range Wireless and Voice Remote with TV Controls" price={39.00} rating={5} image="https://images-na.ssl-images-amazon.com/images/I/81lsA5v8EIL._AC_SL1500_.jpg" />
+                <Product id={234234} title="Roku Streaming Stick+ | HD/4K/HDR Streaming Device with Long-range Wireless and Voice Remote with TV Controls" price={39.00} rating={5} image="https://images-na.ssl-images-amazon.com/images/I/81lsA5v8EIL._AC_SL1500_.jpg" />
+            </div>
+            <div className={classes.home__row}>
+                <Product id={234234} title="Roku Streaming Stick+ | HD/4K/HDR Streaming Device with Long-range Wireless and Voice Remote with TV Controls" price={39.00} rating={5} image="https://images-na.ssl-images-amazon.com/images/I/81lsA5v8EIL._AC_SL1500_.jpg" />
+            </div>
         </div>
     )
 }
