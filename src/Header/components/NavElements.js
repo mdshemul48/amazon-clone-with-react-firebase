2144d2df03f5b467daa9f25b2a3071148eacef82
MD Shimul <mdshemul480@gmail.com>
2021-06-17 16:05:20 +0600

    adding all nav links and text

---
+++ src/Header/components/NavElements.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+// castom components
+import NavLink from './Navlink'
+import classes from "./NavElements.module.css"
+const NavElements = () => {
+    return (
+        <div className={classes.header__nav}>
+            {/* 1st link */}
+            <NavLink to="/login" firstText="Hello Sam" secondText="Sign In" />
+
+            {/* 2nd link */}
+            <NavLink to="/" firstText="Returns" secondText="& Orders" />
+
+
+            {/* 3rd link */}
+            <NavLink to="/" firstText="Your" secondText="Prime" />
+
+
+            {/* 4th link */}
+
+
+        </div>
+    )
+}
+
+export default NavElements
